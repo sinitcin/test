33c8359bcb51e10835eb95b4897a50a152a61aee
sinitcin <antony@email.su>
2019-03-10 15:38:21 +0300

    Добавил Rocket и Gracefull, в котором дополнительно улучшил совместимость с Windows

---
+++ build.rs
@@ -0,0 +1,83 @@
+use std::env;
+use std::fs::{self, DirEntry};
+use std::io::prelude::*;
+use std::path::Path;
+
+fn get_exedir(env_path: &str, level: u8) -> String {
+    let mut curr_level = 0;
+    let mut use_dir = true;
+    let mut out_dir: String = env_path
+        .chars()
+        .map(|x| {
+            if x == '\\' {
+                curr_level += 1;
+                if curr_level == level {
+                    use_dir = false;
+                }
+            };
+            if use_dir {
+                x
+            } else {
+                '\0'
+            }
+        }).collect();
+    out_dir.retain(|c| c != '\0');
+    out_dir
+}
+
+fn visit_dirs(dir: &Path, cb: &mut FnMut(&DirEntry)) -> std::io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn get_relevant_path(base_path: &str, other_path: &str) -> String {
+    other_path.trim_left_matches(base_path).to_owned()
+}
+
+fn recurse_copy(out_dir: &str, proj_dir: &str, other_dir: &str) -> Result<(), std::io::Error> {
+    let path = Path::new(&other_dir);
+    visit_dirs(path, &mut |dir_entry: &DirEntry| {
+        // Получили путь к файлу
+        let dir = &dir_entry.clone();
+        let template_path = dir.path();
+
+        // Получим его путь относительно основной директории
+        let relevant_path = &template_path.to_string_lossy();
+        let relevant_path = get_relevant_path(&proj_dir, relevant_path);
+        println!("{:?}", relevant_path);
+
+        let dir_path = Path::new(&relevant_path).parent().unwrap();
+        let _ = fs::create_dir_all(format!("{}{}", out_dir, dir_path.to_string_lossy())).unwrap_or(());
+        fs::copy(&template_path, format!("{}{}", out_dir, relevant_path)).unwrap();
+    })?;
+    Ok(())
+}
+
+fn main() -> Result<(), std::io::Error> {
+    // Получаем переменные
+    let env_out_dir = env::var("OUT_DIR").unwrap();
+    // let env_out_dir = "C:\\sci_questionnaire\\target\\debug\\build";
+    let out_dir = get_exedir(&env_out_dir, 4);
+    let proj_dir = get_exedir(&env_out_dir, 2);
+    let templates_dir = format!("{}\\templates", proj_dir);
+    let static_dir = format!("{}\\static", proj_dir);
+
+    let mut file = std::fs::File::create("foo.txt")?;
+    file.write_all(out_dir.as_bytes())?;
+    file.write_all(&static_dir.as_bytes())?;
+
+    // Сканируем директории шаблонов
+    recurse_copy(&out_dir, &proj_dir, &templates_dir)?;
+    recurse_copy(&out_dir, &proj_dir, &static_dir)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+#![feature(plugin, decl_macro, proc_macro_hygiene)]
+
+#[macro_use] 
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate rocket_multipart_form_data;
+extern crate graceful;
+extern crate serde_json;
+#[macro_use] 
+extern crate serde_derive;
+
+use std::io;
+use std::path::{Path, PathBuf};
+use rocket::http::RawStr;
+#[allow(unused_imports)]
+use rocket::request::{Form, FromFormValue, Request};
+use rocket::response::NamedFile;
+use rocket::response::Redirect;
+use rocket_contrib::*;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
+use std::thread;
+
+use graceful::SignalGuard;
+
+static STOP: AtomicBool = AtomicBool::new(false);
+
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[catch(404)]
+fn not_found(_req: &Request) -> io::Result<NamedFile> {
+    NamedFile::open("static/404.html")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let signal_guard = SignalGuard::new();
+
+    let handle = thread::spawn(|| {
+        println!("Worker thread started. Type Ctrl+C to stop.");
+        while !STOP.load(Ordering::Acquire) {
+            rocket::ignite()
+                .mount("/", routes![index, files])
+                .register(catchers![not_found])
+                .launch();
+        }
+        println!("Bye.");
+    });
+
+    signal_guard.at_exit(move |sig| {
+        println!("Signal {} received.", sig);
+        STOP.store(true, Ordering::Release);
+        handle.join().unwrap();
+    });
 }
